9b5c1038610f7dfe2d2cfe26698e396d3bc4e541
Isshou <37011117+martin-kudlicka@users.noreply.github.com>
2012-04-08 09:29:37 +0000

    + casino dialog

---
+++ source/casinodialog.h
@@ -0,0 +1,21 @@
+#ifndef CASINODIALOG_H
+#define CASINODIALOG_H
+
+#include <QtGui/QDialog>
+#include "ui_casinodialog.h"
+
+#include "../casino/common/casinointerface.h"
+
+class CasinoDialog : public QDialog
+{
+	Q_OBJECT
+
+	public:
+		CasinoDialog(const CasinoInterface *pCasino, QWidget *pParent = NULL, Qt::WindowFlags pFlags = 0);
+
+	private:
+		const CasinoInterface *_ciCasino;
+		Ui::qdCasino _qdcCasinoDialog;
+}; // CasinoDialog
+
+#endif // CASINODIALOG_H
