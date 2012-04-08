80fa6391cf76804662d5618570ae18d5e4cd7658
Isshou <Isshou@bde5b2e8-4aca-8845-b5bf-8f2c23458600>
2012-04-08 22:44:35 +0000

    + recognize cash

--- casino/unibet/unibet.cpp
+++ casino/unibet/unibet.cpp
@@ -4,8 +4,25 @@
 # include <process.h>
 # define _UNICODE
 # include <tchar.h>
+#endif
+#undef MAX_PATH
+#include "../../3rdparty/tesseract/api/baseapi.h"
+#include "../../3rdparty/tesseract/vs2010/include/leptonica/allheaders.h"
+#include "../../3rdparty/tesseract/ccutil/strngs.h"
 
+#ifdef Q_WS_WIN
 LPCTSTR CAPTION_ROULETTE = _T("Roulette");
+
+HINSTANCE Unibet::_hiInstance = NULL;
+
+BOOL WINAPI DllMain(__in HINSTANCE hinstDLL, __in DWORD fdwReason, __in LPVOID lpvReserved)
+{
+	if (fdwReason == DLL_PROCESS_ATTACH) {
+		Unibet::_hiInstance = hinstDLL;
+	} // if
+
+	return TRUE;
+} // DllMain
 
 BOOL CALLBACK Unibet::EnumWindowsProc(__in HWND hwnd, __in LPARAM lParam)
 {
@@ -14,9 +31,9 @@ BOOL CALLBACK Unibet::EnumWindowsProc(__in HWND hwnd, __in LPARAM lParam)
 
 	bool bRoulette = _tcsncmp(tcWindowText, CAPTION_ROULETTE, _tcslen(CAPTION_ROULETTE)) == 0;
 	if (bRoulette) {
-		HWND hwRoulette = FindWindowEx(hwnd, NULL, NULL, CAPTION_ROULETTE);
-		if (hwRoulette) {
-			*reinterpret_cast<bool *>(lParam) = true;
+		HWND *hwRoulette = reinterpret_cast<HWND *>(lParam);
+		*hwRoulette = FindWindowEx(hwnd, NULL, NULL, CAPTION_ROULETTE);
+		if (*hwRoulette) {
 			return FALSE;
 		} // if
 	} // if
@@ -27,20 +44,30 @@ BOOL CALLBACK Unibet::EnumWindowsProc(__in HWND hwnd, __in LPARAM lParam)
 
 const bool Unibet::GameActive() const
 {
-	return _bActive;
+	return _wiWindow != NULL;
 } // GameActive
+
+const float Unibet::GetCash() const
+{
+	QPixmap qpPixmap = GrabWindow(GrabCash);
+	QString qsCash = Recognize(qpPixmap);
+
+	return qsCash.toFloat();
+} // GetCash
 
 #ifdef Q_WS_WIN
 unsigned _stdcall Unibet::GameCheckThread(void *pContext)
 {
 	Unibet *uUnibet = static_cast<Unibet *>(pContext);
 	while (!uUnibet->_bStop) {
-		bool bFound = false;
-		EnumWindows(&Unibet::EnumWindowsProc, reinterpret_cast<LPARAM>(&bFound));
+		HWND hwRoulette = NULL;
+		EnumWindows(&Unibet::EnumWindowsProc, reinterpret_cast<LPARAM>(&hwRoulette));
 
-		if (uUnibet->_bActive != bFound) {
-			uUnibet->_bActive = bFound;
-			emit uUnibet->GameActiveChanged(bFound);
+		if (uUnibet->_wiWindow != hwRoulette) {
+			if (static_cast<bool>(uUnibet->_wiWindow) ^ static_cast<bool>(hwRoulette)) {
+				emit uUnibet->GameActiveChanged(hwRoulette);
+			} // if
+			uUnibet->_wiWindow = hwRoulette;
 		} // if
 
 		Sleep(uUnibet->CHECK_INTERVAL);
@@ -55,13 +82,58 @@ const QString Unibet::GetName() const
 	return "Unibet";
 } // GetName
 
+const QPixmap Unibet::GrabWindow(const eGrab &pPart) const
+{
+	QPixmap qpAll = QPixmap::grabWindow(_wiWindow);
+
+	int iPartHeight, iPartWidth, iPartX, iPartY;
+	switch (pPart) {
+		case GrabCash:
+			iPartX = static_cast<float>(qpAll.width()) / 100 * 39;
+			iPartY = static_cast<float>(qpAll.height()) / 100 * 90.5;
+			iPartWidth = static_cast<float>(qpAll.width()) / 100 * 6;
+			iPartHeight = static_cast<float>(qpAll.height()) / 100 * 2.5;
+	} // switch
+
+	return qpAll.copy(iPartX, iPartY, iPartWidth, iPartHeight);
+} // GrabWindow
+
+const QString Unibet::Recognize(const QPixmap &pPixmap) const
+{
+	QPixmap qpScaled = pPixmap.scaled(pPixmap.size() * PIXMAP_SCALE, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+
+	tesseract::TessBaseAPI tbaTesseract;
+#ifdef Q_WS_WIN
+	char cFilePath[MAX_PATH];
+	GetModuleFileNameA(Unibet::_hiInstance, cFilePath, sizeof(cFilePath));
+#endif
+	tbaTesseract.Init(
+#ifdef Q_WS_WIN
+		cFilePath,
+#else
+		NULL,
+#endif
+		NULL);
+
+	QTemporaryFile qtfTempFile;
+	qtfTempFile.open();
+	qpScaled.save(qtfTempFile.fileName(), "PNG");
+
+	STRING sText;
+	tbaTesseract.ProcessPages(qtfTempFile.fileName().toLocal8Bit(), NULL, 0, &sText);
+
+	return sText.string();
+} // Recognize
+
 Unibet::Unibet() : CasinoInterface()
 {
-	_bActive = false;
 #ifdef Q_WS_WIN
 	_bStop = false;
+#endif
+	_wiWindow = NULL;
 
-	_uiptCheckThread = _beginthreadex(NULL, 0, &Unibet::GameCheckThread, this, 0, NULL);
+#ifdef Q_WS_WIN
+	_beginthreadex(NULL, 0, &Unibet::GameCheckThread, this, 0, NULL);
 #endif
 } // Unibet
 
