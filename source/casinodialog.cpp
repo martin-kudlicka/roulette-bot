80e8db0bcce3bda8ec632efe16766ad6f3f312fe
Isshou <Isshou@bde5b2e8-4aca-8845-b5bf-8f2c23458600>
2012-04-10 15:35:01 +0000

    + start / stop casino playing according to casino game active

--- source/casinodialog.cpp
+++ source/casinodialog.cpp
@@ -9,7 +9,39 @@ CasinoDialog::CasinoDialog(const CasinoInterface *pCasino, QWidget *pParent /* N
 	setWindowTitle(windowTitle() += pCasino->GetName());
 
 	RefreshStatus();
+
+	connect(pCasino, SIGNAL(GameActiveChanged(const bool &)), SLOT(on_ciCasino_GameActiveChanged(const bool &)));
 } // CasinoDialog
+
+const bool CasinoDialog::IsPlaying() const
+{
+	return !_qdcCasinoDialog.qpbStart->isEnabled();
+} // IsPlaying
+
+const void CasinoDialog::on_ciCasino_GameActiveChanged(const bool &pActive) const
+{
+	if (pActive) {
+		if (!IsPlaying()) {
+			_qdcCasinoDialog.qpbStart->setEnabled(true);
+		} // if
+	} else {
+		if (IsPlaying()) {
+			on_qpbStop_clicked();
+		} // if
+	} // if else
+} // on_ciCasino_GameActiveChanged
+
+const void CasinoDialog::on_qpbStart_clicked(bool checked /* false */) const
+{
+	_qdcCasinoDialog.qpbStart->setEnabled(false);
+	_qdcCasinoDialog.qpbStop->setEnabled(true);
+} // on_qpbStart_clicked
+
+const void CasinoDialog::on_qpbStop_clicked(bool checked /* false */) const
+{
+	_qdcCasinoDialog.qpbStop->setEnabled(false);
+	_qdcCasinoDialog.qpbStart->setEnabled(_ciCasino->GameActive());
+} // on_qpbStop_clicked
 
 const void CasinoDialog::RefreshStatus() const
 {
