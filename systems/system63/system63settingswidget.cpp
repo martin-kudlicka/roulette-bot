8cdc3cd67004a6141c56fd70fade5deab1ccb27c
Isshou <37011117+martin-kudlicka@users.noreply.github.com>
2012-04-10 20:01:55 +0000

    + count manual progression * edit only progression value or sequence at once

--- systems/system63/system63settingswidget.cpp
+++ systems/system63/system63settingswidget.cpp
@@ -6,14 +6,44 @@ const void System63SettingsWidget::LoadSettings() const
 
 	if (_s63sSettings->GetProgressionType() == System63Settings::ProgressionTypeAuto) {
 		_qwsSettingsWidget.qrbProgressionAuto->setChecked(true);
+		on_qrbProgressionAuto_clicked();
 	} else {
 		_qwsSettingsWidget.qrbProgressionManual->setChecked(true);
+		on_qrbProgressionManual_clicked();
 	} // if else
 	_qwsSettingsWidget.qsbProgression->setValue(_s63sSettings->GetProgressionAutoValue());
 	_qwsSettingsWidget.qleProgression->setText(_s63sSettings->GetProgressionManualSequence());
 	_qwsSettingsWidget.qsbSameDozenColumnProgression->setValue(_s63sSettings->GetSameDozenColumnProgression());
 	_qwsSettingsWidget.qcbProgressionDozenColumnNotChanged->setChecked(_s63sSettings->GetProgressionDozenColumnNotChanged());
 } // LoadSettings
+
+const void System63SettingsWidget::on_qrbProgressionAuto_clicked(bool checked /* false */) const
+{
+	_qwsSettingsWidget.qsbProgression->setEnabled(true);
+	_qwsSettingsWidget.qleProgression->setEnabled(false);
+} // on_qrbProgressionAuto_clicked
+
+const void System63SettingsWidget::on_qrbProgressionManual_clicked(bool checked /* false */) const
+{
+	_qwsSettingsWidget.qsbProgression->setEnabled(false);
+	_qwsSettingsWidget.qleProgression->setEnabled(true);
+} // on_qrbProgressionManual_clicked
+
+const void System63SettingsWidget::on_qsbProgression_valueChanged(int i) const
+{
+	QString qsSequence;
+	int iProgression = 1;
+	for (int iValue = 0; iValue < i; iValue++) {
+		if (!qsSequence.isEmpty()) {
+			qsSequence += PROGRESSION_SEQUENCE_SEPARATOR;
+		} // if
+
+		qsSequence += QString::number(iProgression);
+		iProgression *= 3;
+	} // for
+
+	_qwsSettingsWidget.qleProgression->setText(qsSequence);
+} // on_qsbProgression_valueChanged
 
 const void System63SettingsWidget::SaveSettings() const
 {
