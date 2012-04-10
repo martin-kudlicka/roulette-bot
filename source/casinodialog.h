3b934c6537f05303a4f0d08ea6fc892d001a9d4e
Isshou <37011117+martin-kudlicka@users.noreply.github.com>
2012-04-10 15:50:52 +0000

    + prepare casino for new play

--- source/casinodialog.h
+++ source/casinodialog.h
@@ -10,19 +10,20 @@ class CasinoDialog : public QDialog
 	Q_OBJECT
 
 	public:
-		CasinoDialog(const CasinoInterface *pCasino, QWidget *pParent = NULL, Qt::WindowFlags pFlags = 0);
+		CasinoDialog(CasinoInterface *pCasino, QWidget *pParent = NULL, Qt::WindowFlags pFlags = 0);
 
 	private:
-		const CasinoInterface *_ciCasino;
+		bool _bStop;
+		CasinoInterface *_ciCasino;
 		Ui::qdCasino _qdcCasinoDialog;
 
 		const bool IsPlaying() const;
 		const void RefreshStatus() const;
 
 	private slots:
-		const void on_ciCasino_GameActiveChanged(const bool &pActive) const;
-		const void on_qpbStart_clicked(bool checked = false) const;
-		const void on_qpbStop_clicked(bool checked = false) const;
+		const void on_ciCasino_GameActiveChanged(const bool &pActive);
+		const void on_qpbStart_clicked(bool checked = false);
+		const void on_qpbStop_clicked(bool checked = false);
 }; // CasinoDialog
 
 #endif // CASINODIALOG_H
