d0e40a4663bacdb105010106ae90a54ca4b19908
Isshou <Isshou@bde5b2e8-4aca-8845-b5bf-8f2c23458600>
2012-04-12 19:32:52 +0000

    * more accurate statistics with progressions

--- systems/common/systeminterface.h
+++ systems/common/systeminterface.h
@@ -12,18 +12,22 @@ class SystemInterface : public QObject
 
 	public:
 		enum eSpinResult {
-			SpinResultNoBet,
-			SpinResultWon,
-			SpinResultLost
+			SpinResultNoBet = 1,
+			SpinResultWon = 2,
+			SpinResultLost = 4,
+			SpinResultProgression = 8
 		}; // eSpinResult
+		Q_DECLARE_FLAGS(qfSpinResults, eSpinResult)
 
-		virtual const eSpinResult AnalyzeSpin(const quint8 &pSpin) = 0;
+		virtual const qfSpinResults AnalyzeSpin(const quint8 &pSpin) = 0;
 		virtual const void CloseSettings(const QWidget *pSettings, const bool &pSave) const = 0;
 		virtual const PlayCmn::tBetHash GetBet() = 0;
 		virtual const QString GetName() const = 0;
 		virtual QWidget *GetSettings() = 0;
 		virtual const void Reset() = 0;
 }; // SystemInterface
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(SystemInterface::qfSpinResults)
 
 Q_DECLARE_INTERFACE(SystemInterface, SYSTEM_INTERFACE);
 
