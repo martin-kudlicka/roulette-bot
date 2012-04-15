e53cb548b07f1aa5cedf258cb425fa1e24a58357
isshou <isshou@bde5b2e8-4aca-8845-b5bf-8f2c23458600>
2012-04-15 17:40:14 +0000

    + initialize random number generator

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
+#include <QtCore/QTime>
 
 int main(int argc, char *argv[])
 {
 	QApplication qaApplication(argc, argv);
 	MainWindow mwMainWindow;
+
+	qsrand(QTime::currentTime().msec());
 
 	mwMainWindow.show();
 	return qaApplication.exec();
