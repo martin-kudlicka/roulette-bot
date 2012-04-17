867b462e8ca21b6b0502987b1031ad7ec376966a
isshou <37011117+martin-kudlicka@users.noreply.github.com>
2012-04-17 12:42:58 +0000

    * same in row information in statistics instead of color count

--- systems/martingale/martingalestatisticswidget.cpp
+++ systems/martingale/martingalestatisticswidget.cpp
@@ -5,31 +5,44 @@ const void MartingaleStatisticsWidget::Increment(QLabel *pLabel) const
 	pLabel->setText(QString::number(pLabel->text().toInt() + 1));
 } // Increment
 
-const void MartingaleStatisticsWidget::Increment(const eColorCounter &pCounter) const
+const void MartingaleStatisticsWidget::Increment(const eCounter &pCounter) const
 {
 	switch (pCounter) {
-		case ColorCounterBlack:
-			Increment(_qwmswWidget.qlBlackCount);
-			break;
-		case ColorCounterRed:
-			Increment(_qwmswWidget.qlRedCount);
+	case Counter1SameInRow:
+		Increment(_qwmswWidget.ql1SameColorInRow);
+		break;
+	case Counter2SameInRow:
+		Increment(_qwmswWidget.ql2SameColorInRow);
+		break;
+	case Counter3SameInRow:
+		Increment(_qwmswWidget.ql3SameColorInRow);
+		break;
+	case Counter4SameInRow:
+		Increment(_qwmswWidget.ql4SameColorInRow);
+		break;
+	case Counter5SameInRow:
+		Increment(_qwmswWidget.ql5SameColorInRow);
+		break;
+	case Counter6SameInRow:
+		Increment(_qwmswWidget.ql6SameColorInRow);
+		break;
+	case Counter7SameInRow:
+		Increment(_qwmswWidget.ql7SameColorInRow);
+		break;
+	case Counter8SameInRow:
+		Increment(_qwmswWidget.ql8SameColorInRow);
+		break;
+	case Counter9SameInRow:
+		Increment(_qwmswWidget.ql9SameColorInRow);
+		break;
+	case Counter10SameInRow:
+		Increment(_qwmswWidget.ql10SameColorInRow);
 	} // switch
 } // Increment
 
-const void MartingaleStatisticsWidget::SetMaxSameInRow(QLabel *pLabel, const int &pValue) const
+const void MartingaleStatisticsWidget::SetMaxSameInRow(const int &pValue) const
 {
-	pLabel->setText(QString::number(pValue));
-} // SetMaxSameInRow
-
-const void MartingaleStatisticsWidget::SetMaxSameInRow(const eMaxCounter &pCounter, const int &pValue) const
-{
-	switch (pCounter) {
-		case MaxCounterBlackInRow:
-			SetMaxSameInRow(_qwmswWidget.qlMaxBlackInRow, pValue);
-			break;
-		case MaxCounterRedInRow:
-			SetMaxSameInRow(_qwmswWidget.qlMaxRedInRow, pValue);
-	} // switch
+	_qwmswWidget.qlMaxSameColorInRow->setText(QString::number(pValue));
 } // SetMaxSameInRow
 
 MartingaleStatisticsWidget::MartingaleStatisticsWidget(QWidget *pParent /* NULL */, Qt::WindowFlags pFlags /* 0 */) : QWidget(pParent, pFlags)
